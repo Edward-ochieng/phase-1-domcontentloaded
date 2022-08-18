e6f640923a6170039751b5b26bf5b82db1e6f925
Edward <edward.ochieng@student.moringaschool.com>
2022-08-18 16:32:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+const p = document.querySelector('p');
+
+function ianisabitch(){
+    p.textContent = 'This is really cool!'
+}
+
+document.addEventListener('DOMContentLoaded', ianisabitch)
